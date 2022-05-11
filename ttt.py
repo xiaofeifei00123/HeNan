930e5ab55f026385831528af0bcd1d22390a34f3
xiaofeifei00123 <2951985709@qq.com>
2022-05-11 15:50:12 +0800

    测试

---
+++ ttt.py
@@ -0,0 +1,11 @@
+#!/home/fengxiang/anaconda3/envs/wrfout/bin/python
+# -*- encoding: utf-8 -*-
+'''
+Description:
+测试github
+-----------------------------------------
+Author           :Forxd
+Version          :1.0
+Time：2022/05/11/ 15:49
+'''
+
